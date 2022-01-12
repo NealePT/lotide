d4a72eca86f40fc907fd3bb35858ea3f9e288261
Neale Taylor <nealeptaylor@gmail.com>
2022-01-12 13:44:19 -0500

    added countOnly.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -26,26 +26,6 @@ const assertArraysEqual = function(array1, array2) {
 }
 
 
-// const assertArraysEqual = function(array1, array2) {
-//   if (array1 === array2) {
-//     console.log((`✅✅✅Assertion Passed: ${array1} === ${array2}`));
-//   }
-//   if (array1 == null || array2 == null) {
-//     console.log((`❌❌❌Assertion Failed: ${array1} !== ${array2}`));
-//   }
-//   if (array1.length !== array2.length) {
-//     console.log((`❌❌❌Assertion Failed: ${array1} !== ${array2}`));
-//   }
-//   for (var i = 0; i < array1.length; ++i) {
-//     if (array1[i] !== array2[i]) {
-//       console.log((`❌❌❌Assertion Failed: ${array1} !== ${array2}`));
-//     } 
-//   }
-//   console.log((`✅✅✅Assertion Passed: ${array1} === ${array2}`));
-// };
-
-
-
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, "3"]);
 assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);

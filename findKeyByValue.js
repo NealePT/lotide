5eec47e482686a847819d1b185e130b9f2e709ce
Neale Taylor <nealeptaylor@gmail.com>
2022-01-12 16:20:19 -0500

    added findKeyByValue:

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(object, value) {
+  let key = Object.keys(object)
+  let values = Object.values(object);
+  for (i = 0; i < key.length; i++) {
+    if (values[i] === value) {
+      return key[i];
+    }
+  }
+}
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama");

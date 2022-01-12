4b39a551fb7d8c081632a215af635e72d332958d
Neale Taylor <nealeptaylor@gmail.com>
2022-01-12 15:15:29 -0500

    added letter positions, modified countletters

--- countLetters.js
+++ countLetters.js
@@ -9,12 +9,13 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(string) {
   const results = {};
-  string = string.replace(/\s/g, "");
   for (const letter of string) {
-    if (results[letter]) {
-      results[letter] += 1;
-    } else {
-      results[letter] = 1;
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
     }
   }
   return results;

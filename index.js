d0f5249c756880e45dbe6cc8df144842bfe4916f
Neale Taylor <nealeptaylor@gmail.com>
2022-01-17 18:52:06 -0500

    added all functions to index

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
 const head   = require('./head');
-const tail   = require('./tail');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
 const middle = require('./middle');
+const tail   = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail: tail,
+  takeUntil: takeUntil,
+  without: without
 };

09e5a82936bcc6026ffbeb4d03bc829c4254521e
Neale Taylor <nealeptaylor@gmail.com>
2022-01-17 14:25:06 -0500

    added eqArrays test case and linted head and tail

--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 
-function eqArrays(array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1 === array2) return true;
-  if (array1 == null || array2 == null) return false;
+  if (array1 === null || array2 === null) return false;
   if (array1.length !== array2.length) return false;
-  for (var i = 0; i < array1.length; ++i) {
+  for (let i = 0; i < array1.length; ++i) {
     if (array1[i] !== array2[i]) return false;
   }
   return true;
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
 
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
